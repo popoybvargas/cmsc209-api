8c6a7076be28ca1f4400195a6781186d66afb5c3
Popoy Vargas <popoyvargas@me.com>
2021-05-02 15:48:41 +0800

    Add cors middleware.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const router = require('./routers/usersRouter');
@@ -11,6 +12,7 @@ const writeoffsRouter = require('./routers/writeoffsRouter');
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json(), express.urlencoded({ extended: true }));
 
